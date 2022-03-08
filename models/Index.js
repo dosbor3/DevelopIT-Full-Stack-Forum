a688cd97419e0a69b7705b25a791f9403be9fd56
I am Billie <dosbor3@gmail.com>
2022-03-07 21:04:30 -0600

    heroku

--- models/Index.js
+++ models/Index.js
@@ -1,5 +1,5 @@
 const User = require('./User');
-const Post = require('./new');
+const Post = require('./Post');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 const Topic = require('./Topic');
